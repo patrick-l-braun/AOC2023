e3c4c08e298c2ef14e8099a2d44bfc2a4b5eba62
Patrick Braun <63162120+patrick-l-braun@users.noreply.github.com>
2023-12-06 21:57:14 +0000

    Created main header

---
+++ AOC2023/include.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <sstream>
+#include <string> // incluedes find and rfind
+#include <fstream>
+#include <vector>
+#include <map>
+
+
+using std::string;
+using std::cout;    
+using std::cin;
+using std::vector;
+using std::map;
