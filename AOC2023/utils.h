29e884edab198b47d13ab7bdeda899e1514b12ef
Patrick Braun <63162120+patrick-l-braun@users.noreply.github.com>
2023-12-14 21:18:41 +0000

    Added some nice templated printing functions for vectors and maps. Will definitely use this more. Understand the reason for generics or templates a little more

--- AOC2023/utils.h
+++ AOC2023/utils.h
@@ -5,4 +5,23 @@ namespace utils {
 	vector<string> split(const string& s, char sep = ' ');
 	vector<string> getLines(const string& filename);
 	vector<int> convertToInts(const vector<string>& a);
+	
+	template<typename T>
+	void print(const vector<T>& array, char sep = '\n', const string& message="") {
+		for (const auto& item : array) {
+			if (message == "")
+				cout << message;
+			cout << item << sep;
+		}
+		cout << "\n";
+	}
+	template<typename T, typename U>
+	void print(const map<T, U>& mapToPrint, char sep = '\n', const string& message = "") {
+		for (const std::pair<T,U>& item : mapToPrint) {
+			if (message == "")
+				cout << message;
+			cout << item.first << ": " << item.second << sep;
+		}
+		cout << "\n";
+	}
 }
