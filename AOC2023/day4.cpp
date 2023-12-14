d9d1b53864e5691b30358782d136085746121098
Patrick Braun <63162120+patrick-l-braun@users.noreply.github.com>
2023-12-14 13:41:59 +0000

    Finished part2. Setting up the testing for each part really helped. Also barely had to change anything due to how modular I made it.

--- AOC2023/day4.cpp
+++ AOC2023/day4.cpp
@@ -4,13 +4,17 @@ namespace day4 {
 
 	using utils::split;
 	using utils::convertToInts;
+
 	int part1(const string& filename) {
 		vector<string> lines = utils::getLines(filename);
 		int totalPoints{};
 		for (const auto& line : lines) {
+			// split on the :
 			auto gameNumberAndData = split(line, ':');
 			string gameNumber = split(gameNumberAndData[0])[1];
 			string data = gameNumberAndData[1];
+
+			// extract the data
 			auto Numbers = split(data, '|');
 			vector<int> winningNumbers = convertToInts(split(Numbers[0]));
 			vector<int> myNumbers = convertToInts(split(Numbers[1]));
@@ -18,30 +22,65 @@ namespace day4 {
 			// set of winning numbers.
 			std::set<int> setOfWinners;
 
-
-			cout << line << "\n";
-			cout << gameNumber << "\n";
 			for (auto c : winningNumbers) {
-				cout << c << " ";
 				setOfWinners.insert(c);
 			}
-			cout << "\n";
 
 			int winningNumberCount{};
 			for (auto c : myNumbers) {
-				cout << c << " ";
 				if (setOfWinners.find(c) != setOfWinners.end())
 					++winningNumberCount;
 
 			}
-			cout << "\n";
 			totalPoints += static_cast<int>(std::pow(2, winningNumberCount - 1));
 		}
 
 		return totalPoints;
 	}
-	int part2(const string&) {
-		return 0;
+	int part2(const string& filename) {
+		vector<string> lines = utils::getLines(filename);
+		int totalCards{};
+		vector<int> numberOfCards(lines.size(), 1);
+
+		for (const auto& line : lines) {
+			// split on the :
+			auto gameNumberAndData = split(line, ':');
+			// split into Game and Num. Then get Num. Convert to int and -1 for 0 indexing
+			int gameIndex = stoi(split(gameNumberAndData[0])[1]) - 1;
+			string data = gameNumberAndData[1];
+
+			// extract the data
+			auto Numbers = split(data, '|');
+			vector<int> winningNumbers = convertToInts(split(Numbers[0]));
+			vector<int> myNumbers = convertToInts(split(Numbers[1]));
+
+			// set of winning numbers.
+			std::set<int> setOfWinners;
+
+			for (auto c : winningNumbers) {
+				setOfWinners.insert(c);
+			}
+
+			int winningNumberCount{};
+			for (auto c : myNumbers) {
+				if (setOfWinners.find(c) != setOfWinners.end())
+					++winningNumberCount;
+
+			}
+
+			// Get new cards based on number of winning numbers.
+			for (int i = gameIndex + 1; i <= gameIndex + winningNumberCount; i++) {
+				if (i < lines.size()) {
+					// for each copy of current card we get more of the later cards
+					numberOfCards[i] += numberOfCards[gameIndex];
+				}
+			}
+
+			totalCards += numberOfCards[gameIndex];
+		}
+		return totalCards;
+		
+		
 	}
 	void main() {
 		int answer = part1("day4test.txt");
@@ -50,8 +89,10 @@ namespace day4 {
 		cout << part1("day4.txt") << "\n";
 		
 		// part2
-		int answer = part2("day4test.txt");
-		cout << answer << "\n";
-		cout <<( (answer == 30) ? "PASS" : "FAIL") << "\n";
+		int answer2 = part2("day4test.txt");
+		cout << answer2 << "\n";
+		cout <<( (answer2 == 30) ? "PASS" : "FAIL") << "\n";
+
+		cout << part2("day4.txt") << "\n";
 	}
 }
