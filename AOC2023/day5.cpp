d44d7becc887d8472a92a525afa2ea390bb752c5
Patrick Braun <63162120+patrick-l-braun@users.noreply.github.com>
2023-12-18 22:15:09 +0000

    added some initial logid on ranges. Seems to work except for updating the ranges that are only partially updated

--- AOC2023/day5.cpp
+++ AOC2023/day5.cpp
@@ -7,10 +7,41 @@ namespace day5 {
 	using utils::print;
 	constexpr int numberOfMaps = 7;
 
+	struct Range {
+		ll start{};
+		ll length{};
+	};
 	struct Mapping {
 		ll destinationRangeStart{};
 		ll sourceRangeStart{};
 		ll rangeLength{};
+
+		Range getIntersection(Range otherRange) const {
+			Range intersection;
+			if (otherRange.start < sourceRangeStart)
+				intersection.start = sourceRangeStart;
+			else
+				intersection.start = otherRange.start;
+			
+			ll otherRangeEnd = otherRange.start + otherRange.length;
+			ll mappingRangeEnd = sourceRangeStart + rangeLength;
+			if (otherRangeEnd < mappingRangeEnd) {
+				intersection.length = otherRangeEnd - intersection.start;
+			}
+			else {
+				intersection.length = mappingRangeEnd - intersection.start;
+			}
+			return intersection;
+		}
+
+		Range convert(Range range) const {
+			Range intersection = getIntersection(range);
+
+			ll diff = sourceRangeStart - destinationRangeStart;
+			intersection.start = intersection.start - diff;
+			return intersection;
+
+		}
 	};
 	class ConvertMapping {
 	public:
@@ -28,21 +59,43 @@ namespace day5 {
 		ll convert(int mappingNumber, ll number) {
 			for (const Mapping& map : maps[mappingNumber]) {
 				// check if the mapping applies
-				if (number > map.sourceRangeStart && number < map.sourceRangeStart + map.rangeLength)
+				if (number >= map.sourceRangeStart && number < map.sourceRangeStart + map.rangeLength)
 					//convert
 					return number - map.sourceRangeStart + map.destinationRangeStart;
 			}
 			return number;
 		}
+
+		vector<Range> convertRanges(int mappingNumber, vector<Range>& ranges) {
+			vector<Range> outputs;
+			for (const Range& range : ranges) {
+				for (const Mapping& map : maps[mappingNumber]) {
+					Range r = map.convert(range);
+					if (r.length > 0) {
+						outputs.push_back(r);
+					}
+					else {
+						//outputs.push_back(range);
+					}
+				}
+			}
+			return outputs;
+		}
 	private:
 		vector<vector<Mapping>> maps;
 	};
 
-	ll part1(const string& filename) {
+	ll part1(const string& filename, int mode) {
 		vector<string> lines = utils::getLines(filename);
 		print(lines);
 		vector<ll> seedNumbers = convertToLl(split(split(lines[0], ':')[1]));
-		print(seedNumbers);
+		vector<Range> seedRanges;
+		if (mode == 2) {
+			for (int i = 0; i < seedNumbers.size(); i+=2){
+				seedRanges.push_back({ seedNumbers[i], seedNumbers[i + 1] });
+			}
+		}
+		cout << seedNumbers.size() << "\n";
 
 		ConvertMapping converter;
 
@@ -52,30 +105,76 @@ namespace day5 {
 				continue;
 			if (line[line.size() - 1] == ':') {
 				++mapCounter;
-				cout << mapCounter << "\n";
 				continue;
 			}
 			converter.addLine(mapCounter, line);
 		}
 
 		ll closestLocation = std::numeric_limits<ll>::max();
-		for (ll num : seedNumbers) {
-			ll current = num;
-			cout << num << " ";
-			for (int i = 0; i < numberOfMaps; ++i) {
-				current = converter.convert(i, current);
-				cout << " -> " << current;
+		int j = 0;
+		if (mode == 1) {
+			for (ll num : seedNumbers) {
+				cout << j << " / " << seedNumbers.size() << " | ";
+				ll current = num;
+				cout << num << " ";
+				for (int i = 0; i < numberOfMaps; ++i) {
+					current = converter.convert(i, current);
+					cout << " -> " << current;
+				}
+				cout << "\n";
+				closestLocation = std::min(closestLocation, current);
+				j++;
 			}
-			cout << "\n";
-			closestLocation = std::min(closestLocation, current);
+		}
+		if (mode == 2) {
+			vector<Range> ranges = seedRanges;
+			for (Range range : ranges) {
+				vector<Range> current = { range };
+				for (int i = 0; i < numberOfMaps; ++i) {
+					current = converter.convertRanges(i, current);
+				}
+				for (auto r : current) {
+					closestLocation = std::min(closestLocation, r.start);
+				}
+
+			}
 		}
 		return closestLocation;
 	}
 	void main() {
-		ll answer = part1("day5test.txt");
-		cout << answer << " " << (answer == 35 ? "PASS" : "FAIL") << "\n";
+		//ll answer = part1("day5test.txt", 1);
+		//cout << answer << " " << (answer == 35 ? "PASS" : "FAIL") << "\n";
 
-		ll answerForMain = part1("day5.txt");
-		cout << answerForMain << "\n";
+		//ll answerForMain = part1("day5.txt", 1);
+		//cout << answerForMain << "\n";
+
+		ll answer2 = part1("day5test.txt", 2);
+		cout << answer2 << " " << (answer2 == 46 ? "PASS" : "FAIL") << "\n";
+		//ll answerForMain2 = part1("day5.txt", 2);
+		//cout << answerForMain2 << "\n";
+
+		Range r = { 74, 14 };
+		
+		Mapping m = { 45, 77, 23 };
+		Range i = m.getIntersection(r);
+		cout << i.start << " " << i.length << "\n";
+		i = m.convert(r);
+		cout << i.start << " " << i.length << "\n";
+
+
+		Mapping otherM = { 68, 64, 13 };
+		Range otherI = otherM.getIntersection(r);
+		cout << otherI.start << " " << otherI.length << "\n";
+		otherI = otherM.convert(r);
+		cout << otherI.start << " " << otherI.length << "\n";
+
+
+		Mapping irrelaventM = { 81, 45, 19 };
+		otherI = irrelaventM.getIntersection(r);
+		cout << otherI.start << " " << otherI.length << "\n";
+		otherI = irrelaventM.convert(r);
+		cout << otherI.start << " " << otherI.length << "\n";
+
+
 	}
 }
